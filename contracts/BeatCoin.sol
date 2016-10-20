f52085c68885b789fbbf45b8670ccd5f0fc2ee4e
Manuel Araoz <manuelaraoz@gmail.com>
2016-10-20 20:22:17 -0300

    fix compilation errors

--- contracts/BeatCoin.sol
+++ contracts/BeatCoin.sol
@@ -16,32 +16,24 @@ contract BeatCoin is CrowdsaleToken {
   uint public constant REGISTRATION_PRICE = 15000;
   uint public constant SONG_PRICE = 5000;
 
-  // structure with the info of an approved future payment
   struct Order {
     address payer;
-    string store;
-    string item;
     uint value;
     bool exists;
   }
   // mapping from name of store to its orders, mapped by item
-  mapping(string => mapping(string => Order)) public orders;
+  mapping(string => mapping(string => Order)) orders;
 
 
-  function orderItem(string store, string item, uint value) {
+  function placeOrder(string store, string item, uint value) {
     mapping(string => Order) storeOrders = orders[store];
 
-    Order order = Order();
-    order.payer = msg.sender;
-    order.store = store;
-    order.item = item;
-    order.value = value;
-    order.exists = true;
+    Order memory order = Order(msg.sender, value, true);
 
     transfer(this, value);
 
     storeOrders[item] = order;
-    OrderPlaced(order);
+    OrderPlaced(order.payer, store, item, value);
   }
 
   function completeOrder(string store, string item, address account) {
@@ -50,18 +42,18 @@ contract BeatCoin is CrowdsaleToken {
     
     executePayment(order, account);
     
-    OrderCompleted(order);
+    OrderPlaced(order.payer, store, item, order.value);
     delete orders[store][item];
   }
 
-  function executePayment(Order order, address account) {
+  function executePayment(Order order, address account) internal {
     // this.tranfer makes the call external, making
     // msg.sender == this inside transfer
     this.transfer(account, order.value);
   }
 
-  event OrderPlaced(Order order);
-  event OrderCompleted(Order order);
+  event OrderPlaced(address payer, string store, string item, uint value);
+  event OrderCompleted(address payer, string store, string item, uint value);
 
   /*
    * registerSong
@@ -73,7 +65,7 @@ contract BeatCoin is CrowdsaleToken {
   function registerSong(string song, string namespace) {
     address artist = msg.sender;
 
-    orderItem(namespace, song, REGISTRATION_PRICE);
+    placeOrder(namespace, song, REGISTRATION_PRICE);
   }
 
 
@@ -85,6 +77,6 @@ contract BeatCoin is CrowdsaleToken {
    */
   function purchaseSong(string song) {
     address purchaser = msg.sender;
-    orderItem(song, 'download', SONG_PRICE);
+    placeOrder(song, 'download', SONG_PRICE);
   }
 }
